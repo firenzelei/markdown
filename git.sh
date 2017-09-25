80dc2c03c89e52fa7c402fa1d64ab710903669f6
lei1991 <firenzelei@163.com>
2017-09-25 18:52:16 +0800

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,7 @@
+wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/3bc53220cb2dcf709f7a027a3f526befd021d858/contrib/completion/git-completion.bash 
+echo "" >> ~/.bashrc && echo "source ~/.git-completion.bash" >> ~/.bashrc
+source ~/.git-completion.bash
+
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.cm commit
